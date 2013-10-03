14129846c7cf43b78e4c6dcfcc6946794ed23c96
TheRealColbyMarquez <colbymarquez@msn.com>
2013-10-03 08:44:14 -0400

    10/3/13

---
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            int guesses = 7;
+            int number;
+            
+
+            Console.WriteLine("Choose a number between 1 and 100");
+            Console.WriteLine("Is your number 50?");
+            string response = Console.ReadLine();
+            if(response == "no")
+            {
+                
+        }
+    }
+}
